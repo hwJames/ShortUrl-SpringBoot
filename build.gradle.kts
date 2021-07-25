9cfaedd92616af1688c678b71eec5f9af91a20ee
KJH <kjhg5852@naver.com>
2021-07-25 11:47:13 +0900

    React Gradle 연동

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.Locale
 
 plugins {
     id("org.springframework.boot") version "2.5.3"
@@ -23,6 +24,60 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+var webappDir = "$projectDir/src/main/front"
+
+sourceSets {
+    main {
+        resources {
+            srcDirs("$webappDir/build", "$projectDir/src/main/resources")
+        }
+    }
+}
+
+// region DuplicatesStrategy Error
+inline fun <reified C> Project.configure(name: String, configuration: C.() -> Unit) {
+    (this.tasks.getByName(name) as C).configuration()
+}
+
+configure<ProcessResources>("processResources") {
+    duplicatesStrategy = DuplicatesStrategy.WARN
+    dependsOn("buildReact")
+}
+
+tasks.register("jvmProcessResources", Copy::class) {
+    duplicatesStrategy = DuplicatesStrategy.WARN
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.INHERIT }
+// endregion
+
+tasks.register("buildReact", Exec::class) {
+    dependsOn("installReact")
+    workingDir(webappDir)
+    inputs.dir(webappDir)
+    group = BasePlugin.BUILD_GROUP
+
+    if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("windows")) {
+        commandLine("yarn.cmd", "run", "build")
+    } else {
+        commandLine( "yarn", "run", "build")
+    }
+}
+
+tasks.register("installReact", Exec::class) {
+    workingDir(webappDir)
+    inputs.dir(webappDir)
+    group = BasePlugin.BUILD_GROUP
+
+    if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("windows")) {
+        commandLine("yarn.cmd", "audit", "fix")
+        commandLine("yarn.cmd", "install")
+    } else {
+        commandLine("yarn", "audit", "fix")
+        commandLine("yarn", "install")
+    }
 }
 
 tasks.withType<KotlinCompile> {
