c97b5019eac33134c99e7c817a6d2dddc2450420
KJH <kjhg5852@naver.com>
2021-07-28 15:27:21 +0900

    CI 수정 - 3

--- src/main/kotlin/studio/hwjames/shorturl/controller/MainController.kt
+++ src/main/kotlin/studio/hwjames/shorturl/controller/MainController.kt
@@ -9,6 +9,6 @@ class MainController {
 
     @GetMapping("/api/hello")
     fun main(): String {
-        return "안녕하세용!! 현재 서버시간은 " + Date() + "입니다. \n";
+        return "안녕하세요 현재 서버시간은 " + Date() + "입니다. \n";
     }
 }
